8badaa6517c56f7cdab9ebcf4f67e4a77fac64f7
Chris Jimison <chris@merigo.co>
2018-04-26 15:21:31 -0700

    fixed mix warning for Elixir 1.5+

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Riakc.Mixfile do
   use Mix.Project
 
-  @version File.read!("VERSION") |> String.strip
+  @version File.read!("VERSION") |> String.trim()
 
   def project do
     [app: :riakc,
